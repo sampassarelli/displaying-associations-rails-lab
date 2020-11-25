7730519f2a5ac6d77ed9ebbbd49eca36b6edfb08
sampassarelli <passarelli.sam@gmail.com>
2020-11-25 07:01:12 -0600

    commit tree 0cd4f4d92fe3eeda2dc133eb0202bfcdbec83274

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,11 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
+    @songs = Song.all
   end
 
   def new
